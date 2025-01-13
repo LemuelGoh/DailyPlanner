f61fb25ae640ac4a0d7466ec7c5acc55858e9eaf
Lemuel Goh <chunyong2008@hotmail.com>
2025-01-13 22:01:20 +0800

    Done all the fucking register login forget password and password recovery interface

--- spering-html/js/custom.js
+++ spering-html/js/custom.js
@@ -17,4 +17,43 @@ function getCurrentYear() {
     $("#displayDate").html(currentYear);
 }
 
+function showRegisterForm() {
+    document.getElementById('login-form').classList.add('hidden');
+    document.getElementById('register-form').classList.remove('hidden');
+    document.getElementById('forget-password-form').classList.add('hidden');
+    document.getElementById('password-recovery-form').classList.add('hidden');
+}
+
+function showLoginForm() {
+    document.getElementById('login-form').classList.remove('hidden');
+    document.getElementById('register-form').classList.add('hidden');
+    document.getElementById('forget-password-form').classList.add('hidden');
+    document.getElementById('password-recovery-form').classList.add('hidden');
+}
+
+function showForgetPasswordForm() {
+    document.getElementById('login-form').classList.add('hidden');
+    document.getElementById('register-form').classList.add('hidden');
+    document.getElementById('forget-password-form').classList.remove('hidden');
+    document.getElementById('password-recovery-form').classList.add('hidden');
+}
+
+function showPasswordRecoveryForm() {
+    document.getElementById('login-form').classList.add('hidden');
+    document.getElementById('register-form').classList.add('hidden');
+    document.getElementById('forget-password-form').classList.add('hidden');
+    document.getElementById('password-recovery-form').classList.remove('hidden');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+document.querySelector('.btn-register').addEventListener('click', showRegisterForm);
+document.querySelectorAll('.btn-login').forEach(button => button.addEventListener('click', showLoginForm));
+document.querySelector('.btn-forget-password').addEventListener('click', showForgetPasswordForm);
+document.querySelector('.btn-password-recovery').addEventListener('click', showPasswordRecoveryForm);
+});
+  
+function clearInput(inputId) {
+    document.getElementById(inputId).value = '';
+}
+  
 getCurrentYear();
